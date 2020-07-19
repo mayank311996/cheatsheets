aa4b4089d2dbca847eafee067e4f0db1c6ea6946
mayank311996 <mayankvadsola1996@gmail.com>
2020-07-18 22:15:51 -0400

    Adding chapter 5

--- Books/Abhishek_Thakur_book_1/book.py
+++ Books/Abhishek_Thakur_book_1/book.py
@@ -967,6 +967,64 @@ def mcc(y_true, y_pred):
 
 
 
+#################################################################################################
+# Chapter 5: Arranging Machine Learning Projects
+#################################################################################################
+
+# src/train.py
+import joblib
+import pandas as pd
+from sklearn import metrics
+from sklearn import tree
+
+def run(fold):
+    # read the training data with folds
+    df = pd.read_csv("../input/mnist_train_folds.csv")
+    # training data is where kfold is not equal to provided fold, also note that we reset the index
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    # validation data is where kfold is equal to provided fold
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+    # drop the label column from dataframe and convert it to a numpy array by using .values.
+    # target is label column in the dataframe
+    x_train = df_train.drop("label", axis=1).values
+    y_train = df_train.label.values
+    # similarly, for validation, we have
+    x_valid = df_valid.drop("label", axis=1).values
+    y_valid = df_valid.label.values
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
