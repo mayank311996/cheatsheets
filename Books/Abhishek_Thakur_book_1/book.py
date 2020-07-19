c3a80ced28842a3025dc68f1f0050cb573d6414f
mayank311996 <mayankvadsola1996@gmail.com>
2020-07-19 11:04:47 -0400

    editing chapter 5

--- Books/Abhishek_Thakur_book_1/book.py
+++ Books/Abhishek_Thakur_book_1/book.py
@@ -1015,6 +1015,113 @@ if __name__ == "__main__":
 TRAINING_FILE = "../input/mnist_train_folds.csv"
 MODEL_OUTPUT = "../models/"
 
+### src/train.py v2
+import os
+import config
+import joblib
+import pandas as pd
+from sklearn import metrics
+from sklearn import tree
+
+def run(fold):
+    # read the training data with folds
+    df = pd.read_csv(config.TRAINING_FILE)
+    # training data is where kfold is not equal to provided fold, also note that we reset the index
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    # validation data is where kfold is equal to provided fold
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+    # drop the label column from dataframe and convert it to a numpy array by using .values.
+    # target is label column in the dataframe
+    x_train = df_train.drop("label", axis=1).values
+    y_train = df_train.label.values
+    # similarly, for validation, we have
+    x_valid = df_valid.drop("label", axis=1).values
+    y_valid = df_valid.label.values
+    # initialize simple decision tree classifier from sklearn
+    clf = tree.DecisionTreeClassifier()
+    # fit the model on training data
+    clf.fit(x_train, y_train)
+    # create predictions for validation samples
+    preds = clf.predict(x_valid)
+    # calculate & print accuracy 
+    accuracy = metrics.accuracy_score(y_valid, preds)
+    print(f"Fold={fold}, Accuracy={accuracy}")
+    # save the model 
+    joblib.dump(
+        clf,
+        os.path.join(config.MODEL_OUTPUT, f"dt_{fold}.bin")
+    )
+
+if __name__ == "__main__":
+    run(fold=0)
+    run(fold=1)
+    run(fold=2)
+    run(fold=3)
+    run(fold=4)
+
+### src/train.py v3
+import argparse
+.
+.
+.
+
+if __name__ == "__main__":
+    # Initialize ArgumentParser class of argparse
+    parser = argparse.ArgumentParser()
+    # add the different arguments you need and their type 
+    # currently, we only need fold
+    parser.add_argument(
+        "--fold",
+        type=int
+    )
+    # read the arguments from the command line 
+    args = parser.parse_args()
+    # run the folds specified by command line arguments 
+    run(fold=args.fold)
+
+### shell script
+#!/bin/sh
+python train.py --fold 0
+python train.py --fold 1
+python train.py --fold 2
+python train.py --fold 3
+python train.py --fold 4
+
+### Running shell script
+sh run.sh
+
+### src/model_dispatcher.py
+from sklearn import tree
+
+models = {
+    "decision_tree_gini": tree.DecisionTreeClassifier(
+        criterion="gini"
+    ),
+    "decision_tree_entropy": tree.DecisionTreeClassifier(
+        criterion="entropy"
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
