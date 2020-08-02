b6a2ec3fa11fdd65e3b76242d4413f9a7a9b631d
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-02 17:36:39 -0400

    editing main.py

--- AIEngineering/deep_learning_computer_vision/main.py
+++ AIEngineering/deep_learning_computer_vision/main.py
@@ -115,15 +115,113 @@ model.fit(
 print(tf.distribute.get_strategy())
 
 
+# by default strategy, data sits on CPU and computation happens on GPUs
+# so at each iteration data is transferred from CPU to GPU
+
+# %tensorboard --logdir /tmp/logs
+# !nvidia-smi
 
 
+def create_model_deep():
+    """
+    Creates a deep learning model
+    :return: A deeper CNN model
+    """
+    return tf.keras.Sequential(
+        [
+            tf.keras.layers.Conv2D(31, 3, activation="relu", input_shape=(28, 28, 1)),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Conv2D(64, 3, activation="relu"),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation="softmax")
+        ]
+    )
 
 
+strategy = tf.distribute.MirroredStrategy()  # Copies data to GPUs
+with strategy.scope():
+    model_deep = create_model_deep()
+    model_deep.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["sparse_categorical_accuracy"]
+    )
 
+    logdir = os.path.join("/tmp/logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1, profile_batch=3)
+    checkpointer = tf.keras.callbacks.ModelCheckpoint(
+        filepath="/tmp/fmnist_model.hdf5",
+        verbose=1,
+        save_best_only=True
+    )
+    csv_logger = tf.keras.callbacks.CSVLogger("/tmp/history.log")
 
+    train_dataset, test_dataset, val_dataset = get_dataset()
+    train_dataset.cache()
+    val_dataset.cache()
 
+    model.fit(
+        train_dataset,
+        epochs=10,
+        steps_per_epoch=10,
+        validation_data=val_dataset,
+        callbacks=[tensorboard_callback, checkpointer, csv_logger]
+    )
+    model_deep.save("/tmp/final_fmnist_model.hdf5")
+    print(tf.distribute.get_strategy())
 
+# !ls -alrt /tmp/final_fmnist_model.hdf5
+# history.history
 
+plt.plot(history.history["sparse_categorical_accuracy"])
+plt.plot(history.history["val_sparse_categorical_accuracy"])
 
+# from tensorboard import notebook
+# notebook.list()
+# notebook.display(port=6006, height=1000)
+
+# if you want to run it on specific CPU
+with tf.device("/CPU:0"):
+    model_deep = create_model_deep()
+    model_deep.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["sparse_categorical_accuracy"]
+    )
+
+    logdir = os.path.join("/tmp/logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1, profile_batch=3)
+
+    train_dataset, test_dataset, val_dataset = get_dataset()
+    train_dataset.cache()
+    val_dataset.cache()
+
+    model.fit(
+        train_dataset,
+        epochs=5,
+        steps_per_epoch=10,
+        validation_data=val_dataset,
+        callbacks=[tensorboard_callback]
+    )
+    model_deep.save("/tmp/final_fmnist_model.hdf5")
+    print(tf.distribute.get_strategy())
+
+tf.keras.backend.clear_session()
+fmnist_load = tf.keras.models.load_model("/tmp/final_fmnist_model.hdf5", compile=False)
+
+for test_sample in fm_test.take(10):
+    image, label = test_sample[0], test_sample[1]
+    img = tf.keras.preprocessing.image.img_to_array(image)
+    img = np.expand_dims(img, axis=0)
+    img /= 255.
+    pred = fmnist_load.predict(img)
+    plt.figure()
+    plt.imshow(image.numpy()[:, :, 0].astype(np.float32), cmap=plt.get_cmap("gray"))
+    plt.show()
+    print(f"Actual label: {info.features['label'].names[label.numpy()]}")
+    print(f"Predicted label: {info.features['label'].names[np.argmax(pred)]}")
 
 
