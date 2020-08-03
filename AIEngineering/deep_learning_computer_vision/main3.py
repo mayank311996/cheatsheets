24ed35cb7c52ba454f29b0e188af276fd23fd53d
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-02 23:59:51 -0400

    adding main3.py

---
+++ AIEngineering/deep_learning_computer_vision/main3.py
@@ -0,0 +1,13 @@
+########################################################################
+# Transfer Learning - Image Classification using TensorFlow
+########################################################################
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_hub as hub  # contains pretrained computer vision and nlp models
+import os
+import numpy as np
+import tensorflow_datasets as tfds
+import warnings
+warnings.filterwarnings("ignore")
+
+
