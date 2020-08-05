f465ce75c0a6ee500e7347ff9c3df7903c1514be
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-05 13:30:08 -0400

    editing chapter2.py

--- Books/nlpia_manning/src/chapter2.py
+++ Books/nlpia_manning/src/chapter2.py
@@ -4,6 +4,12 @@
 
 import numpy as np
 import pandas as pd
+import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.util import ngrams
+import nltk
+nltk.download("stopwords")
 
 #########################################################################################
 sentence = "Thomas Jefferson began building Monticello at the age of 26."
@@ -36,6 +42,85 @@ sorted(sentence_bow.items())
 
 
 #########################################################################################
+df = pd.DataFrame(pd.Series(dict([(token, 1) for token in sentence.split()
+                                  ])), columns=["sent"]).T
+
+sentences = "Thomas Jefferson began building Monticello at the" \
+            "age of 26.\n"
+sentences += "Construction was done mostly by local masons and" \
+             "carpenters.\n"
+sentences += "He moved into the South Pavilion in 1770.\n"
+sentences += "Turning Monticello into a neoclassical masterpiece" \
+             "was Jefferson's obsession."
+corpus = {}
+for i, sent in enumerate(sentences.split('\n')):
+    corpus[f'sent{i}'] = dict((tok, 1) for tok in sent.split())
+df = pd.DataFrame.from_records(corpus).fillna(0).astype(int).T
+
+
+#########################################################################################
+df = df.T
+df.sent0.dot(df.sent1)
+df.sent0.dot(df.sent2)
+df.sent0.dot(df.sent3)
+
+print([(k, v) for (k, v) in (df.sent0 & df.sent3).items() if v])
+
+
+#########################################################################################
+sentence = "Thomas Jefferson began building Monticello at the" \
+           "age of 26."
+tokens = re.split(r'[-\s.,;!?]+', sentence)
+
+pattern = re.compile(r'[-\s.,;!?]+')
+tokens = pattern.split(sentence)
+print([x for x in tokens if x and x not in '-\t\n.,;!?'])
+
+
+#########################################################################################
+tokenizer = RegexpTokenizer(r'\w+|$[0-9.]+|\S+')
+tokenizer.tokenize(sentence)
+
+
+#########################################################################################
+sentence = "Monticello wan't designated as UNESCO World Heritage" \
+           "Site until 1987."
+tokenizer = TreebankWordTokenizer()
+tokenizer.tokenize(sentence)
+
+
+#########################################################################################
+pattern = re.compile(r'[-\s.,;!?]+')
+tokens = pattern.split(sentence)
+tokens = [x for x in tokens if x and x not in '-\t\n.,;!?']
+
+list(ngrams(tokens, 2))
+list(ngrams(tokens, 3))
+two_grams = list(ngrams(tokens, 2))
+[" ".join(x) for x in two_grams]
+
+
+#########################################################################################
+stop_words = ["a", "an", "the", "on", "of", "off", "this", "is"]
+tokens = ["the", "house", "is", "on", "fire"]
+tokens_without_stopwords = [x for x in tokens if x not in stop_words]
+
+
+#########################################################################################
+stop_words = nltk.corpus.stopwords.words("english")
+len(stop_words)
+print(stop_words[:7])
+print([sw for sw in stop_words if len(sw) == 1])
+
+
+#########################################################################################
+
+
+
+
+
+
+
 
 
 
