d278654045cadf999270c11b2634e58fe9b882c9
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-05 22:59:40 -0400

    editing chapter2.py

--- Books/nlpia_manning/src/chapter2.py
+++ Books/nlpia_manning/src/chapter2.py
@@ -5,17 +5,26 @@
 import numpy as np
 import pandas as pd
 import re
+from collections import Counter
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.util import ngrams
 import nltk
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS \
+    as sklearn_stop_words
+from nltk.stem import WordNetLemmatizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nlpia.data.loaders import get_data
+from nltk.tokenize import casual_tokenize
+from sklearn.naive_bayes import MultinomialNB
+nltk.download("wordnet")
 nltk.download("stopwords")
+
 
 #########################################################################################
 sentence = "Thomas Jefferson began building Monticello at the age of 26."
 sentence.split()
 str.split(sentence)
-
 
 #########################################################################################
 token_sequence = str.split(sentence)
@@ -33,13 +42,11 @@ print(onehot_vectors)
 pd.DataFrame(onehot_vectors, columns=vocab)
 # df[df == 0] = ''
 
-
 #########################################################################################
 sentence_bow = {}
 for token in sentence.split():
     sentence_bow[token] = 1
 sorted(sentence_bow.items())
-
 
 #########################################################################################
 df = pd.DataFrame(pd.Series(dict([(token, 1) for token in sentence.split()
@@ -57,7 +64,6 @@ for i, sent in enumerate(sentences.split('\n')):
     corpus[f'sent{i}'] = dict((tok, 1) for tok in sent.split())
 df = pd.DataFrame.from_records(corpus).fillna(0).astype(int).T
 
-
 #########################################################################################
 df = df.T
 df.sent0.dot(df.sent1)
@@ -65,7 +71,6 @@ df.sent0.dot(df.sent2)
 df.sent0.dot(df.sent3)
 
 print([(k, v) for (k, v) in (df.sent0 & df.sent3).items() if v])
-
 
 #########################################################################################
 sentence = "Thomas Jefferson began building Monticello at the" \
@@ -76,18 +81,15 @@ pattern = re.compile(r'[-\s.,;!?]+')
 tokens = pattern.split(sentence)
 print([x for x in tokens if x and x not in '-\t\n.,;!?'])
 
-
 #########################################################################################
 tokenizer = RegexpTokenizer(r'\w+|$[0-9.]+|\S+')
 tokenizer.tokenize(sentence)
-
 
 #########################################################################################
 sentence = "Monticello wan't designated as UNESCO World Heritage" \
            "Site until 1987."
 tokenizer = TreebankWordTokenizer()
 tokenizer.tokenize(sentence)
-
 
 #########################################################################################
 pattern = re.compile(r'[-\s.,;!?]+')
@@ -99,12 +101,10 @@ list(ngrams(tokens, 3))
 two_grams = list(ngrams(tokens, 2))
 [" ".join(x) for x in two_grams]
 
-
 #########################################################################################
 stop_words = ["a", "an", "the", "on", "of", "off", "this", "is"]
 tokens = ["the", "house", "is", "on", "fire"]
 tokens_without_stopwords = [x for x in tokens if x not in stop_words]
-
 
 #########################################################################################
 stop_words = nltk.corpus.stopwords.words("english")
@@ -112,10 +112,85 @@ len(stop_words)
 print(stop_words[:7])
 print([sw for sw in stop_words if len(sw) == 1])
 
+#########################################################################################
+len(sklearn_stop_words)
+len(stop_words)
+len(stop_words.union(sklearn_stop_words))
+len(stop_words.intersection(sklearn_stop_words))
 
 #########################################################################################
+tokens = ["House", "Visitor", "Center"]
+normalized_tokens = [x.lower() for x in tokens]
+print(normalized_tokens)
 
 
+#########################################################################################
+def stem(phrase):
+    return " ".join([re.findall('^(.*ss|.*?)(s)?$', word)
+                     [0][0].strip("'") for word in phrase.lower().split()])
+
+
+print(stem('houses'))
+print(stem("Doctor House's calls"))
+
+#########################################################################################
+lemmatizer = WordNetLemmatizer()
+lemmatizer.lemmatize("better")
+lemmatizer.lemmatize("better", pos="a")
+lemmatizer.lemmatize("good", pos="a")
+lemmatizer.lemmatize("goods", pos="a")
+lemmatizer.lemmatize("goods", pos="n")
+lemmatizer.lemmatize("goodness", pos="n")
+lemmatizer.lemmatize("best", pos="a")
+
+#########################################################################################
+sa = SentimentIntensityAnalyzer()
+print(sa.lexicon)
+print([(tok, score) for tok, score in sa.lexicon.items() if " " in tok])
+
+sa.polarity_scores(
+    text="Python is very readable and it's great for NLP."
+)
+sa.polarity_scores(
+    text="Python is not a bad choice for most applications."
+)
+
+corpus = [
+    "Absolutely perfect! Love it! :-) :-) :-)",
+    "Horrible! Completely useless. :(",
+    "It was OK. Some good and some bad things."
+]
+for doc in corpus:
+    scores = sa.polarity_scores(doc)
+    print(f"{scores['compound']}:{doc}")
+
+#########################################################################################
+movies = get_data("hutto_movies")
+movies.head().round(2)
+movies.describe().round(2)
+
+pd.set_option("display.width", 75)
+
+bags_of_words = []
+for text in movies.text:
+    bags_of_words.append(Counter(casual_tokenize(text)))
+df_bows = pd.DataFrame.from_records(bags_of_words)
+df_bows = df_bows.fillna(0).astype(int)
+print(df_bows.shape)
+df_bows.head()
+df_bows.head()[list(bags_of_words[0].keys())]
+
+nb = MultinomialNB()
+nb = nb.fit(df_bows, movies.sentiment > 0)
+movies['predicted_sentiment'] = nb.predict_proba(df_bows)*8 - 4
+movies['error'] = (movies.predict_sentiment - movies.sentiment).abs()
+print(movies.error.mean().round(1))
+movies['sentiment_ispositive'] = (movies.sentiment > 0).astype(int)
+movies['predicted_ispositive'] = (movies.predict_sentiment > 0).astype(int)
+movies["sentiment predicted_sentiment sentiment_ispositive" \
+       " predicted_ispositive".split()].head(8)
+
+#########################################################################################
 
 
 
