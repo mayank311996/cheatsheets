40135356fda7610ca0062a67febdc10574c0055b
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-06 20:44:51 -0400

    editing chapter3.py

--- Books/nlpia_manning/src/chapter3.py
+++ Books/nlpia_manning/src/chapter3.py
@@ -5,10 +5,13 @@
 from nltk.tokenize import TreebankWordTokenizer
 from collections import Counter
 from collections import OrderedDict
-from nlpia.data.loader import kite_text
+from nlpia.data.loader import kite_text, kite_history
+from nltk.corpus import brown
 import nltk
 import copy
+import math
 nltk.download('stopwords', quiet=True)
+nltk.download('brown')
 
 #########################################################################################
 sentence = "The faster Harry go to the store, the faster Harry," \
@@ -77,6 +80,75 @@ for doc in docs:
     doc_vectors.append(vec)
 
 #########################################################################################
+
+
+def cosine_sim(vec1, vec2):
+    """
+    Function to calculate cosine similarity
+    :param vec1: Vector 1
+    :param vec2: Vector 2
+    :return: cosine similarity score
+    """
+    vec1 = [val for val in vec1.values()]
+    vec2 = [val for val in vec2.values()]
+
+    dot_prod = 0
+    for i, v in enumerate(vec1):
+        dot_prod += v*vec2[i]
+
+    mag_1 = math.sqrt(sum([x**2 for x in vec1]))
+    mag_2 = math.sqrt(sum([x**2 for x in vec2]))
+
+    return dot_prod/(mag_1*mag_2)
+
+
+#########################################################################################
+print(brown.words()[:10])
+print(brown.tagged_words()[:5])
+print(len(brown.words()))
+
+puncs = {',', '.', '--', '-', '!', '?', ':', ';', '``', "''", '(', ')', '[', ']'}
+word_list = (x.lower() for x in brown.words() if x not in puncs)
+token_counts = Counter(word_list)
+print(token_counts.most_common(20))
+
+#########################################################################################
+kite_intro = kite_text.lower()
+intro_tokens = tokenizer.tokenize(kite_intro)
+kite_history = kite_history.lower()
+history_tokens = tokenizer.tokenize(kite_history)
+intro_total = len(intro_tokens)
+print(intro_total)
+history_total = len(history_tokens)
+print(history_total)
+
+intro_tf = {}
+history_tf = {}
+intro_counts = Counter(intro_tokens)
+intro_tf['kite'] = intro_counts['kite']/intro_total
+history_counts = Counter(history_tokens)
+history_tf['kite'] = history_counts['kites']/history_total
+print(f"Term frequency of kite in intro is: {intro_tf['kite']}")
+print(f"Term frequency of kite in history is: {history_tf['kite']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
