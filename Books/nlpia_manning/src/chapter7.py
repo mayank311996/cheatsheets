8a50b5bd3758dfe2a90caa340396c70433854524
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-08 23:37:19 -0400

    editing chapter7.py

--- Books/nlpia_manning/src/chapter7.py
+++ Books/nlpia_manning/src/chapter7.py
@@ -2,3 +2,21 @@
 # Chapter 7
 #########################################################################################
 
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.layers import Dense, Dropout, Activation
+
+#########################################################################################
+model = Sequential()
+model.add(
+    Conv1D(
+        filters=16,
+        kernel_size=3,
+        padding='same',
+        activation='relu',
+        strides=1,
+        input_shape=(100, 300)
+    )
+)
