4966055274f095b43e180cc78bf009fe3fc3fa6c
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-11 17:38:41 -0400

    editing chapter9.py

--- Books/nlpia_manning/src/chapter9.py
+++ Books/nlpia_manning/src/chapter9.py
@@ -2,4 +2,37 @@
 # Chapter 9
 #########################################################################################
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, LSTM
+
+#########################################################################################
+MAXLEN = 400
+BATCH_SIZE = 32
+EMBEDDING_DIMS = 300
+EPOCHS = 2
+NUM_NEURONS = 50
+
+model = Sequential()
+model.add(
+    LSTM(
+        NUM_NEURONS,
+        return_sequences=True,
+        input_shape=(MAXLEN, EMBEDDING_DIMS)
+    )
+)
+model.add(
+    Dropout(0.2)
+)
+model.add(
+    Flatten()
+)
+model.add(
+    Dense(1, activation="sigmoid")
+)
+model.compile(
+    "rmsprop",
+    "binary_crossentropy",
+    metrics=["accuracy"]
+)
+print(model.summary())
 
