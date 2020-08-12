d48de88892eb5446ff8f4478776dca3ae2e5a55f
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-12 11:21:18 -0400

    editing chapter9.py listing 9.30

--- Books/nlpia_manning/src/chapter9.py
+++ Books/nlpia_manning/src/chapter9.py
@@ -2,6 +2,7 @@
 # Chapter 9
 #########################################################################################
 
+import sys
 import random
 import numpy as np
 from tensorflow.keras.models import Sequential
@@ -567,6 +568,33 @@ def sample(preds, temperature=1.0):
     preds = exp_preds/np.sum(exp_preds)
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
+
+
+#########################################################################################
+start_index = random.randint(0, len(text) - maxlen - 1)
+for diversity in [0.2, 0.5, 1.0]:
+    print()
+    print("----- diversity:", diversity)
+    generated = ""
+    sentence = text[start_index: start_index + maxlen]
+    generated += sentence
+    print("----- Generating with seed: " + sentence)
+    sys.stdout.write(generated)
+
+    for i in range(400):
+        x = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x[0, t, char_indices[char]] = 1.0
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, diversity)
+        next_char = indices_char[next_index]
+        generated += next_char
+        sentence = sentence[1:] + next_char
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+    print()
+
+#########################################################################################
 
 
 
