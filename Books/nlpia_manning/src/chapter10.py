8bc106983515b637185c619792e1e11697e30e80
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-12 17:31:38 -0400

    editing chapter10.py listing 10.1

--- Books/nlpia_manning/src/chapter10.py
+++ Books/nlpia_manning/src/chapter10.py
@@ -2,4 +2,24 @@
 # Chapter 10
 #########################################################################################
 
+import sys
+import random
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, LSTM, \
+    Embedding, Activation, GRU, Input
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.optimizers import RMSprop
+
+#########################################################################################
+encoder_inputs = Input(
+    shape=(None, input_vocab_size)
+)
+encoder = LSTM(
+    num_neurons,
+    return_state=True
+)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+encoder_states = (state_h, state_c)
+
+#########################################################################################
