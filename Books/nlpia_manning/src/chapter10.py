c7ee1bf952103aaa9e4314745c4327c3a58f90b7
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-12 17:46:57 -0400

    editing chapter10.py listing 10.2

--- Books/nlpia_manning/src/chapter10.py
+++ Books/nlpia_manning/src/chapter10.py
@@ -23,3 +23,38 @@ encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 encoder_states = (state_h, state_c)
 
 #########################################################################################
+decoder_inputs = Input(
+    shape=(None, output_vocab_size)
+)
+decoder_lstm = LSTM(
+    num_neurons,
+    return_sequences=True,
+    return_state=True
+)
+decoder_outputs, _, _ = decoder_lstm(
+    decoder_inputs,
+    initial_state=encoder_states
+)
+decoder_dense = Dense(
+    output_vocab_size,
+    activation="softmax"
+)
+decoder_outputs = decoder_dense(decoder_outputs)
+
+#########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
