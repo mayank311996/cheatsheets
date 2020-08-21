36454a5704a1511a85db0d23119b848577d233cc
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-21 09:09:58 -0400

    editing main7.py

--- AIEngineering/deep_learning_computer_vision/main7.py
+++ AIEngineering/deep_learning_computer_vision/main7.py
@@ -7,10 +7,12 @@
 # is trained where as in second case you do optimize during training.
 
 # !pip install -q tensorflow-model-optimization
-import tensoflow_datasets as tfds
+import tensorflow_model_optimization as tfmot
+import tensorflow_datasets as tfds
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 tf.config.experimental.list_physical_devices()
 
@@ -53,6 +55,91 @@ def scale(image, label):
     image /= 255.0
 
     return image, label
+
+
+def get_dataset(batch_size=256):
+    """
+    Scales and shuffles the datasets
+    :param batch_size: Desired batch size
+    :return: scaled and shuffled train, test and val datasets
+    """
+    train_dataset_scaled = fm_train.map(scale).shuffle(60000).batch(batch_size)
+    test_dataset_scaled = fm_test.map(scale).batch(batch_size)
+    val_dataset_scaled = fm_val.map(scale).batch(batch_size)
+
+    return train_dataset_scaled, test_dataset_scaled, val_dataset_scaled
+
+
+def create_model():
+    """
+    Creates a simple keras model
+    :return: keras model
+    """
+    model = tf.keras.models.Sequential()
+    model.add(
+        tf.keras.layers.Conv2D(
+            64,
+            2,
+            padding="same",
+            activation="relu",
+            input_shape=(28, 28, 1)
+        )
+    )
+    model.add(
+        tf.keras.layers.MaxPooling2D()
+    )
+    model.add(
+        tf.keras.layers.Dropout(0.3)
+    )
+    model.add(
+        tf.keras.layers.Conv2D(
+            128,
+            2,
+            padding="same",
+            activation="relu"
+        )
+    )
+    model.add(
+        tf.keras.layers.MaxPooling2D()
+    )
+    model.add(
+        tf.keras.layers.Dropout(0.3)
+    )
+    model.add(
+        tf.keras.layers.Flatten()
+    )
+    model.add(
+        tf.keras.layers.Dense(256)
+    )
+    model.add(
+        tf.keras.layers.Activation("relu")
+    )
+    model.add(
+        tf.keras.layers.Dense(10)
+    )
+    model.add(
+        tf.keras.layers.Activation("softmax")
+    )
+    return model
+
+
+# quantization stuff is need to be taken care during training only
+# otherwise model definition and other stuff is similar to normal
+# training
+
+model = create_model()
+quantize_model = tfmot.quantization.keras.quantize_model
+q_aware_model = quantize_model(model)
+q_aware_model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+q_aware_model.summary()
+
+logdir = os.path.join("/tmp/logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
 
 
 
