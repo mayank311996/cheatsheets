7b30391258a5d076370cfe8d175e3515ba637196
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-22 23:36:38 -0400

    editing chapter1.py listing 2

--- Books/Deep_Learning_with_TF2_Packt/chapter1.py
+++ Books/Deep_Learning_with_TF2_Packt/chapter1.py
@@ -68,6 +68,84 @@ test_loss, test_acc = model.evaluate(X_test, Y_test)
 print("Test accuracy:", test_acc)
 
 #########################################################################################
+EPOCHS = 50
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2
+RESHAPED = 784
+
+# loading and splitting data
+mnist = tf.keras.mnist
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+# reshaping data
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(60000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+# normalizing input
+X_train /= 255.0
+X_test /= 255.0
+
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# One-hot-encoding
+Y_train = tf.keras.utils.to_categorical(Y_train, NB_CLASSES)
+Y_test = tf.keras.utils.to_categorical(Y_test, NB_CLASSES)
+
+# build the model
+model = tf.keras.models.Sequential()
+model.add(
+    keras.layers.Dense(
+        N_HIDDEN,
+        input_shape=(RESHAPED,),
+        name='dense_layer',
+        activation='relu'
+    )
+)
+model.add(
+    keras.layers.Dense(
+        N_HIDDEN,
+        name='dense_layer2',
+        activation='relu'
+    )
+)
+model.add(
+    keras.layers.Dense(
+        NB_CLASSES,
+        name='dense_layer3',
+        activation='softmax'
+    )
+)
+
+model.summary()
+
+# compiling the model
+model.compile(
+    optimizer='SGD',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# training the model
+model.fit(
+    X_train,
+    Y_train,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    verbose=VERBOSE,
+    validation_split=VALIDATION_SPLIT
+)
+
+# evaluate the model
+test_loss, test_acc = model.evaluate(X_test, Y_test)
+print("Test accuracy:", test_acc)
+
+#########################################################################################
 
 
 
