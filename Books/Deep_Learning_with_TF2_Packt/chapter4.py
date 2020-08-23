9e3d3c7a2536d974712a3aa24234192c5a9dc1c7
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-23 11:04:24 -0400

    editing chapter4.py listing 2

--- Books/Deep_Learning_with_TF2_Packt/chapter4.py
+++ Books/Deep_Learning_with_TF2_Packt/chapter4.py
@@ -123,6 +123,85 @@ print("\nTest score:", score[0])
 print("Test accuracy:", score[1])
 
 #########################################################################################
+IMG_CHANNELS = 3
+IMG_ROWS = 32
+IMG_COLS = 32
+BATCH_SIZE = 128
+EPOCHS = 20
+CLASSES = 10
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+OPTIM = tf.keras.optimizers.RMSprop()
+
+
+def build(input_shape, classes):
+    model = models.Sequential()
+    model.add(
+        layers.Convolution2D(
+            32,
+            (3, 3),
+            activation='relu',
+            input_shape=input_shape
+        )
+    )
+    model.add(
+        layers.MaxPooling2D(
+            pool_size=(2, 2)
+        )
+    )
+    model.add(
+        layers.Dropout(0.25)
+    )
+    model.add(
+        layers.Flatten()
+    )
+    model.add(
+        layers.Dense(
+            512,
+            activation='relu'
+        )
+    )
+    model.add(
+        layers.Dropout(0.5)
+    )
+    model.add(
+        layers.Dense(classes, activation='softmax')
+    )
+
+    return model
+
+
+callbacks = [
+    tf.keras.callbacks.TensorBoard(log_dir='./logs')
+]
+
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer=OPTIM,
+    metrics=['accuracy']
+)
+
+model.fit(
+    X_train,
+    y_train,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    validation_split=VALIDATION_SPLIT,
+    verbose=VERBOSE,
+    callbacks=callbacks
+)
+
+score = model.evaluate(
+    X_test,
+    y_test,
+    batch_size=BATCH_SIZE,
+    verbose=VERBOSE
+)
+
+print("\nTest score:", score[0])
+print("Test accuracy:", score[1])
+
+#########################################################################################
 
 
 
