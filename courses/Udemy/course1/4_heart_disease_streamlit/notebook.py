32df74086d2f2808b978555f8ea93d0ae392da7b
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-26 09:34:55 -0400

    editing notebook.py

--- courses/Udemy/course1/4_heart_disease_streamlit/notebook.py
+++ courses/Udemy/course1/4_heart_disease_streamlit/notebook.py
@@ -152,9 +152,44 @@ fill = Imputer(missing_values=0, strategy='mean', axis=0)
 X_train = fill.fit_transform(X_train)
 X_test = fill.fit_transform(X_test)
 
+
 #########################################################################################
+def fitmodel(x, y, algo_name, algorithm, gridsearchparams, cv):
+    """
+    Trains the model and predicts on test data
+    :param x: data
+    :param y: labels
+    :param algo_name: name of the algorithm
+    :param algorithm: algorithm to be used
+    :param gridsearchparams: grid parameters
+    :param cv: number of cross validations
+    :return: none
+    """
+    np.random.seed(10)
+    x_train, x_test, y_train, y_test = \
+        train_test_split(x, y, test_size=0.2)
+
+    grid = GridSearchCV(
+        estimator=algorithm,
+        param_grid=gridsearchparams,
+        cv=cv,
+        scoring='accuracy',
+        verbose=1,
+        n_jobs=-1
+    )
+
+    grid_result = grid.fit(x_train, y_train)
+    best_params = grid_result.best_params_
+    pred = grid_result.predict(x_test)
+    cm = confusion_matrix(y_test, pred)
+    pickle.dump(grid_result, open(algo_name, 'wb'))
+    print('Best Params:', best_params)
+    print('Classification Report:', classification_report(y_test, pred))
+    print('Accuracy Score:' + str(accuracy_score(y_test, pred)))
+    print('Confusion Matrix:', cm)
 
 
+#########################################################################################
 
 
 
