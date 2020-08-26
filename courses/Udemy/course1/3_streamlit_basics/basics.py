c2577b7e768fa6d446b58bad8dc903e136da770d
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-25 22:23:26 -0400

    editing basics.py

--- courses/Udemy/course1/3_streamlit_basics/basics.py
+++ courses/Udemy/course1/3_streamlit_basics/basics.py
@@ -23,3 +23,47 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
+#########################################################################################
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2)/[50, 50] + [37.76, -122.3],
+    columns=['lat', 'lon']
+)
+
+st.map(map_data)
+
+if st.checkbox['Show Details']:
+    st.dataframe(
+        pd.DataFrame(
+            {
+                '1st col': [1, 2, 3, 4, 5],
+                '2nd col': [352, 6, 32, 6, 2]
+            }
+        )
+    )
+
+#########################################################################################
+df = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+col = st.selectbox('Select the columns to be displayed', df.columns)
+st.line_chart(df[col])
+
+df = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+cols = st.multiselect('Select the columns to be displayed', df.columns)
+st.line_chart(df[cols])
+
+#########################################################################################
+
+
+
+
+
+
+
+
+
+
