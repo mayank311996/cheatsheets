81cf76385122c69208998bb2bcc9ddce58b42818
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-27 22:54:07 -0400

    editing notebook.py

--- courses/Udemy/course1/6_project_diabetes_prediction_AWS_Lambda/notebook.py
+++ courses/Udemy/course1/6_project_diabetes_prediction_AWS_Lambda/notebook.py
@@ -13,6 +13,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, \
     classification_report, accuracy_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import Imputer
 import pickle
 from lightgbm import LGBMClassifier
 print("Library Loaded")
@@ -44,6 +45,8 @@ sns.pairplot(df, hue='Outcome')
 # model to separate between the class
 
 #########################################################################################
+# Feature Engineering
+df.Outcome.value_counts()
 
 cols = df.columns
 print(cols)
@@ -54,6 +57,66 @@ for col in cols:
     print(
         f"# rows in {len(df.loc[df[col] == 0])} with ZERO value: {col}"
     )
+
+X = df.drop('Outcome', axis=1)
+y = df.Outcome
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=10
+)
+
+print('Training Set:', len(X_train))
+print('Test Set:', len(X_test))
+print('Training labels:', len(y_train))
+print('Test labels:', len(y_test))
+
+fill = Imputer(missing_values=0, strategy="mean", axis=0)
+
+X_train = fill.fit_transform(X_train)
+X_test = fill.fit_transform(X_test)
+
+
+#########################################################################################
+def fitmodel(x_train, y_train, x_test, y_test, algo_name, algorithm, gridsearchparams, cv):
+    """
+    Trains the model and predicts on test data
+    :param x_train: train data
+    :param y_train: train labels
+    :param x_test: test data
+    :param y_test: test labels
+    :param algo_name: name of the algorithm
+    :param algorithm: algorithm to be used
+    :param gridsearchparams: grid parameters
+    :param cv: number of cross validations
+    :return: none
+    """
+    np.random.seed(10)
+    grid = GridSearchCV(
+        estimator=algorithm,
+        param_grid=gridsearchparams,
+        cv=cv,
+        scoring='accuracy',
+        verbose=1,
+        n_jobs=-1
+    )
+
+    grid_result = grid.fit(x_train, y_train)
+    best_params = grid_result.best_params_
+    pred = grid_result.predict(x_test)
+    cm = confusion_matrix(y_test, pred)
+    pickle.dump(grid_result, open(algo_name, 'wb'))
+    print('Best Params:', best_params)
+    print('Classification Report:', classification_report(y_test, pred))
+    print('Accuracy Score:' + str(accuracy_score(y_test, pred)))
+    print('Confusion Matrix:', cm)
+
+
+#########################################################################################
+# Model building and evaluation
+
 
 
 
