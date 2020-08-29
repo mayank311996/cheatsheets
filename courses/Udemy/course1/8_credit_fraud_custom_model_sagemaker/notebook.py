7b5a98197db9fd847d7423f10b21dbf057508d39
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-29 13:31:01 -0400

    editing notebook.py

--- courses/Udemy/course1/8_credit_fraud_custom_model_sagemaker/notebook.py
+++ courses/Udemy/course1/8_credit_fraud_custom_model_sagemaker/notebook.py
@@ -57,9 +57,67 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 # we should use stratified split as data is not balanced
 print(x_train.shape, x_test.shape)
 
+
 #########################################################################################
+def fitmodel(x_train, y_train, x_test, y_test, algo_name, algorithm, gridsearchparams, cv):
+    """
+    Trains the model and predicts on test data
+    :param x_train: train data
+    :param y_train: train labels
+    :param x_test: test data
+    :param y_test: test labels
+    :param algo_name: name of the algorithm
+    :param algorithm: algorithm to be used
+    :param gridsearchparams: grid parameters
+    :param cv: number of cross validations
+    :return: none
+    """
+    np.random.seed(10)
+    grid = GridSearchCV(
+        estimator=algorithm,
+        param_grid=gridsearchparams,
+        cv=cv,
+        scoring='accuracy',
+        verbose=1,
+        n_jobs=-1
+    )
+
+    grid_result = grid.fit(x_train, y_train)
+    best_params = grid_result.best_params_
+    pred = grid_result.predict(x_test)
+    cm = confusion_matrix(y_test, pred)
+    pickle.dump(grid_result, open(algo_name, 'wb'))
+    print('Best Params:', best_params)
+    print('Classification Report:', classification_report(y_test, pred))
+    print('Accuracy Score:' + str(accuracy_score(y_test, pred)))
+    print('Confusion Matrix:', cm)
 
 
+#########################################################################################
+# Model Fitting
+# Simple Neural Network
+model = Sequential([
+    Dense(units=16, input_dim=29, activation='relu'),
+    Dense(units=23, activation='relu'),
+    Dropout(0.5),
+    Dense(units=20, activation='relu'),
+    Dense(units=24, activation='relu'),
+    Dense(units=1, activation='sigmoid')
+])
+model.summary()
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+model.fit(
+    x_train,
+    y_train,
+    batch_size=15,
+    epochs=5
+)
+print(model.evaluate(x_test, y_test))
 
 
 
