1c41dfb77716a4c08e8bca0c7a765c266e51ce50
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-31 16:23:17 -0400

    editing handler.py

--- courses/Udemy/course2/2_deploy_sklearn_model/handler.py
+++ courses/Udemy/course2/2_deploy_sklearn_model/handler.py
@@ -1,24 +1,50 @@
 import json
+from sklearn.externals import joblib
+
+model_name = "model.joblib"
+model = joblib.load(model_name)
 
 
-def hello(event, context):
+def predict(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "OK"
     }
+
+    params = event["queryStringParameters"]
+
+    medInc = float(params['medInc'])/100000
+    houseAge = float(params['houseAge'])
+    aveRooms = float(params['aveRooms'])
+    aveBedrms = float(params['aveBedrms'])
+    population = float(params['population'])
+    aveOccup = float(params['aveOccup'])
+    latitude = float(params['latitude'])
+    longitude = float(params['longitude'])
+
+    inputVector = [
+        medInc,
+        houseAge,
+        aveRooms,
+        aveBedrms,
+        population,
+        aveOccup,
+        latitude,
+        longitude
+    ]
+
+    data = [inputVector]
+    predictPrice = model.predict(data)[0] * 100000
+    predictPrice = round(predictPrice, 2)
+    body['predictPrice'] = predictPrice
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        }
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
