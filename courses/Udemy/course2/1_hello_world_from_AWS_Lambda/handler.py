d5bf1fc0741e9f385c591ac2eecb70c0418e015e
mayank311996 <mayankvadsola1996@gmail.com>
2020-08-31 12:57:12 -0400

    editing handler.py

--- courses/Udemy/course2/1_hello_world_from_AWS_Lambda/handler.py
+++ courses/Udemy/course2/1_hello_world_from_AWS_Lambda/handler.py
@@ -3,9 +3,11 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "Go Serverless v1.0! Your hello-world function executed successfully!",
         "input": event
     }
+
+    print(body['message'])
 
     response = {
         "statusCode": 200,
@@ -13,12 +15,3 @@ def hello(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
