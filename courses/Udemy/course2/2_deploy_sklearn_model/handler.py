bd08f7f06fb64510460487d36018c35d0c9d113a
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-01 12:33:52 -0400

    editing handler.py

--- courses/Udemy/course2/2_deploy_sklearn_model/handler.py
+++ courses/Udemy/course2/2_deploy_sklearn_model/handler.py
@@ -10,32 +10,38 @@ def predict(event, context):
         "message": "OK"
     }
 
-    params = event["queryStringParameters"]
+    if 'queryStringParameters' in event.keys():
+        params = event["queryStringParameters"]
 
-    medInc = float(params['medInc'])/100000
-    houseAge = float(params['houseAge'])
-    aveRooms = float(params['aveRooms'])
-    aveBedrms = float(params['aveBedrms'])
-    population = float(params['population'])
-    aveOccup = float(params['aveOccup'])
-    latitude = float(params['latitude'])
-    longitude = float(params['longitude'])
+        medInc = float(params['medInc'])/100000
+        houseAge = float(params['houseAge'])
+        aveRooms = float(params['aveRooms'])
+        aveBedrms = float(params['aveBedrms'])
+        population = float(params['population'])
+        aveOccup = float(params['aveOccup'])
+        latitude = float(params['latitude'])
+        longitude = float(params['longitude'])
 
-    inputVector = [
-        medInc,
-        houseAge,
-        aveRooms,
-        aveBedrms,
-        population,
-        aveOccup,
-        latitude,
-        longitude
-    ]
+        inputVector = [
+            medInc,
+            houseAge,
+            aveRooms,
+            aveBedrms,
+            population,
+            aveOccup,
+            latitude,
+            longitude
+        ]
 
-    data = [inputVector]
-    predictPrice = model.predict(data)[0] * 100000
-    predictPrice = round(predictPrice, 2)
-    body['predictPrice'] = predictPrice
+        data = [inputVector]
+        predictPrice = model.predict(data)[0] * 100000
+        predictPrice = round(predictPrice, 2)
+        body['predictPrice'] = predictPrice
+
+    else:
+        body['message'] = 'queryStringParameters not in event.'
+        # this else is needed to avoid error when CouldWatch ping the
+        # Lambda function because it doesn't have queryStringParameters
 
     response = {
         "statusCode": 200,
