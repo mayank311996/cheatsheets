f60b3a343fc505b79b76eb1b7feb1a432d812a5f
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-06 23:51:17 -0400

    editing sql_with_python.py

---
+++ MySQL/sql_with_python.py
@@ -0,0 +1,8 @@
+#########################################################################################
+# Create and Manipulate SQL Databases with Python
+#########################################################################################
+
+# https://www.freecodecamp.org/news/connect-python-with-sql/
+# https://github.com/thecraigd/Python_SQL
+
+
