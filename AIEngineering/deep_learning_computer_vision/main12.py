70a0eae634fe6bd38a7141892331849158f92cc0
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-06 22:51:42 -0400

    editing main12.py

--- AIEngineering/deep_learning_computer_vision/main12.py
+++ AIEngineering/deep_learning_computer_vision/main12.py
@@ -54,10 +54,42 @@ def load_image_into_numpy_array(path):
 
 
 #########################################################################################
+polar_image_path = '/content/downloads/alone polar bear in arctic/*'
+polar_image_np = []
+for iname in glob.glob(polar_image_path):
+    polar_image_np.append(load_image_into_numpy_array(iname))
 
+plt.rcParams['figure.figsize'] = [14, 7]
+for idx, polar_image_np in enumerate(polar_image_np):
+    plt.subplot(2, 3, idx+1)
+    plt.imshow(polar_image_np)
+plt.show()
 
+gt_boxes = []
+colab_utils.annotate(
+    polar_image_np,
+    box_storage_pointer=gt_boxes
+)
+print(gt_boxes)
+np.save("weights", gt_boxes)
 
+# ls
 
+for idx in range(6):
+    plt.subplot(2, 3, idx+1)
+    image_np_with_annotations = polar_image_np[idx].copy()
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+        image_np_with_annotations,
+        gt_boxes[idx],
+        np.ones(shape=[gt_boxes[idx].shape[0]], dtype=np.int32),
+        np.array([1.0], dtype=np.float32),
+        {1: {'id': 1, 'name': 'polar_bear'}},
+        use_normalized_coordinates=True,
+        min_score_thresh=0.8
+    )
+    plt.imshow(image_np_with_annotations)
+
+#########################################################################################
 
 
 
