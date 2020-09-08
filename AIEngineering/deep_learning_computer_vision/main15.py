ebabfeeaf01f2b8f5cd03f4f900364b38587f2b6
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-07 23:17:44 -0400

    editing main15.py

--- AIEngineering/deep_learning_computer_vision/main15.py
+++ AIEngineering/deep_learning_computer_vision/main15.py
@@ -16,6 +16,7 @@ tf.config.experimental_connect_to_cluster(resolver)
 tf.tpu.experimental.initialize_tpu_system(resolver)
 # strategy = tf.distribute.experimental.TPUStrategy(resolver)
 
+# Running on CPU
 # create some tensors
 a_cpu = tf.constant(
     [
@@ -35,7 +36,30 @@ c_cpu = tf.matmul(a_cpu, b_cpu)
 session = tf.Session()
 print(session.run(c_cpu))
 
+# Running on TPU
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# create some tensors
+a_tpu = tf.constant(
+    [
+        [1.0, 2.0, 3.0],
+        [4.0, 5.0, 6.0]
+    ]
+)
+b_tpu = tf.constant(
+    [
+        [1.0, 2.0],
+        [3.0, 4.0],
+        [5.0, 6.0]
+    ]
+)
+tpu_ops = tf.contrib.tpu.rewrite(tf.matmul, [a_tpu, b_tpu])
 
+session = tf.Session(tpu_add)
+print(session.run(tpu_ops))
+
+
+#########################################################################################
+# taking sparkml logistic regression model and deploying on a TPU
 
 
 
