e35954ceaf17ff49952d027365bc6a9eb1a0d840
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-08 14:33:15 -0400

    editing train.py changing for Mirrored Strategy and DenseNet121

--- AWS/SageMaker/plant_pathology_model_training/train.py
+++ AWS/SageMaker/plant_pathology_model_training/train.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import DenseNet121
+import tensorflow.keras.layers as L
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
+#########################################################################################
 TEST_PATH = "/opt/ml/input/data/test.csv"
 TRAIN_PATH = "/opt/ml/input/data/train.csv"
 IMAGE_DIR = "/opt/ml/input/data/images/"
@@ -11,67 +15,60 @@ OUTPUT_DIR = "/opt/ml/model/"
 test_data = pd.read_csv(TEST_PATH)
 train_data = pd.read_csv(TRAIN_PATH)
 
-IMG_SIZE = (150, 150)
-BATCH_SIZE = 128
+# IMG_SIZE = (150, 150)
+EPOCHS = 50
+strategy = tf.distribute.MirroredStrategy()
+BATCH_SIZE = 32 * strategy.num_replicas_in_sync
+AUTO = tf.data.experimental.AUTOTUNE
 
 
+#########################################################################################
 def format_path(st):
     return IMAGE_DIR + st + '.jpg'
 
 
-def train():
-    image_gen = ImageDataGenerator(
-        rescale=1. / 255,
-        validation_split=0.2
-    )
+def decode_image(filename, label=None, image_size=(512, 512)):
+    bits = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(bits, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.image.resize(image, image_size)
 
-    train_data_gen = image_gen.flow_from_directory(
-        batch_size=BATCH_SIZE,
-        directory=DATA_DIR,
-        subset="training",
-        shuffle=True,
-        target_size=IMG_SIZE,
-        class_mode='sparse'
-    )
-
-    test_data_gen = image_gen.flow_from_directory(
-        batch_size=BATCH_SIZE,
-        directory=DATA_DIR,
-        subset="validation",
-        target_size=IMG_SIZE,
-        class_mode='sparse'
-    )
-
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(*IMG_SIZE, 3)),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(6)
-    ])
-
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(lr=1e-4),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"]
-    )
-
-    model.fit(train_data_gen, epochs=2)
-    test_loss, test_accuracy = model.evaluate(test_data_gen)
-    print(f"Test accuracy: {test_accuracy}")
-
-    model.save(OUTPUT_DIR + "model.h5")
+    if label is None:
+        return image
+    else:
+        return image, label
 
 
+def data_augment(image, label=None):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_flip_up_down(image)
+
+    if label is None:
+        return image
+    else:
+        return image, label
+
+
+def build_lrfn(lr_start=0.00001, lr_max=0.00005,
+               lr_min=0.00001, lr_rampup_epochs=5,
+               lr_sustain_epochs=0, lr_exp_decay=.8):
+    lr_max = lr_max * strategy.num_replicas_in_sync
+
+    def lrfn(epoch):
+        if epoch < lr_rampup_epochs:
+            lr = (lr_max - lr_start) / lr_rampup_epochs * epoch + lr_start
+        elif epoch < lr_rampup_epochs + lr_sustain_epochs:
+            lr = lr_max
+        else:
+            lr = (lr_max - lr_min) *\
+                 lr_exp_decay**(epoch - lr_rampup_epochs
+                                - lr_sustain_epochs) + lr_min
+        return lr
+    return lrfn
+
+
+#########################################################################################
 if __name__ == "__main__":
-
     test_paths = test_data.image_id.apply(format_path).values
     train_paths = train_data.image_id.apply(format_path).values
 
@@ -79,4 +76,57 @@ if __name__ == "__main__":
     train_paths, valid_paths, train_labels, valid_labels = \
         train_test_split(train_paths, train_labels, test_size=0.15, random_state=2020)
 
-    train()
+    train_dataset = (
+        tf.data.Dataset
+            .from_tensor_slices((train_paths, train_labels))
+            .map(decode_image, num_parallel_calls=AUTO)
+            .map(data_augment, num_parallel_calls=AUTO)
+            .repeat()
+            .shuffle(512)
+            .batch(BATCH_SIZE)
+            .prefetch(AUTO)
+    )
+
+    valid_dataset = (
+        tf.data.Dataset
+            .from_tensor_slices((valid_paths, valid_labels))
+            .map(decode_image, num_parallel_calls=AUTO)
+            .batch(BATCH_SIZE)
+            .cache()
+            .prefetch(AUTO)
+    )
+
+    test_dataset = (
+        tf.data.Dataset
+            .from_tensor_slices(test_paths)
+            .map(decode_image, num_parallel_calls=AUTO)
+            .batch(BATCH_SIZE)
+    )
+
+    lrfn = build_lrfn()
+    STEPS_PER_EPOCH = train_labels.shape[0] // BATCH_SIZE
+    lr_schedule = tf.keras.callbacks.LearningRateScheduler(lrfn, verbose=1)
+
+    with strategy.scope():
+        model = tf.keras.Sequential([DenseNet121(input_shape=(512, 512, 3),
+                                                 weights='imagenet',
+                                                 include_top=False),
+                                     L.GlobalAveragePooling2D(),
+                                     L.Dense(train_labels.shape[1],
+                                             activation='softmax')])
+
+        model.compile(optimizer='adam',
+                      loss='categorical_crossentropy',
+                      metrics=['categorical_accuracy'])
+        model.summary()
+
+    history = model.fit(train_dataset,
+                        epochs=EPOCHS,
+                        callbacks=[lr_schedule],
+                        steps_per_epoch=STEPS_PER_EPOCH,
+                        validation_data=valid_dataset)
+
+    # test_loss, test_accuracy = model.evaluate(test_dataset)
+    # print(f"Test accuracy: {test_accuracy}")
+
+    model.save(OUTPUT_DIR + "model_DenseNet121.h5")
