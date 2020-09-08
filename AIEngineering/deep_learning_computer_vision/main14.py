fafd8dd72dbb6c86e5e200e72905e171ff409525
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-07 22:47:22 -0400

    editing main14.py

--- AIEngineering/deep_learning_computer_vision/main14.py
+++ AIEngineering/deep_learning_computer_vision/main14.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import numpy as np
 import os
 import datetime
+import matplotlib.pyplot as plt
 
 #########################################################################################
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist. \
@@ -105,6 +106,62 @@ def create_model():
 
 
 #########################################################################################
+# mkdir /tmp/logs
+
+resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
+    'grpc://' + os.environ['COLAB_TPU_ADDR']
+)  # this fetches the TPU address
+tf.contrib.distribute.initialize_tpu_system(resolver)  # initialize TPU
+strategy = tf.contrib.distribute.TPUStrategy(resolver)
+
+with strategy.scope():
+    model = create_model()
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+        loss='sparse_categorical_crossentropy',
+        metrics=['sparse_categorical_accuracy']
+    )
+
+    logdir = os.path.join(
+        "/tmp/logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    )
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        logdir,
+        histogram_freq=1,
+        profile_batch=3
+    )
+
+    history = model.fit(
+        x_train.astype(np.float32),
+        y_train.astype(np.float32),
+        epochs=20,
+        steps_per_epoch=50,
+        validation_data=(
+            x_test.astype(np.float32),
+            y_test.astype(np.float32)
+        ),
+        validation_freq=5,
+        callbacks=[tensorboard_callback]
+    )
+
+plt.figure(figsize=(6, 8))
+plt.plot(history.history['sparse_categorical_accuracy'])
+plt.plot(history.history['val_sparse_categorical_accuracy'])
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train_acc', 'val_acc'])
+plt.show()
+
+# %tensorboard --logdir /tmp/logs
+
+#########################################################################################
+
+
+
+
+
+
 
 
 
