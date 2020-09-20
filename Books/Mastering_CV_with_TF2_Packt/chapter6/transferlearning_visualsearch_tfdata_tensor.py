b67515a0c739bb11ba5290b60c6bf85da8e6bd05
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-20 12:42:45 -0400

    editing transferlearning_visualsearch_tfdata_tensor.py add directory block

--- Books/Mastering_CV_with_TF2_Packt/chapter6/transferlearning_visualsearch_tfdata_tensor.py
+++ Books/Mastering_CV_with_TF2_Packt/chapter6/transferlearning_visualsearch_tfdata_tensor.py
@@ -8,5 +8,52 @@ from tensorflow.keras.optimizers import SGD, Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
+import pathlib
 
 ##############################################################################
+img_width, img_height = 299, 299
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+NUM_EPOCHS = 5
+batchsize = 10
+num_train_images = 1350
+num_val_images = 156
+
+base_model = InceptionV3(
+    weigths='imagenet',
+    include_top=False,
+    input_shape=(img_height, img_width, 3)
+)
+
+train_dir = '../train'
+val_dir = '../validation'
+train_dir = pathlib.Path(train_dir)
+val_dir = pathlib.Path(val_dir)
+output_dir = '../output'
+
+train_ds = tf.data.Dataset.list_files(str(train_dir/'*/*'))
+val_ds = tf.data.Dataset.list_files(str(val_dir/'*/*'))
+class_list = ["bed", "chair", "sofa"]
+
+for f in train_ds.take(5):
+    print(f.numpy())
+
+
+##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
