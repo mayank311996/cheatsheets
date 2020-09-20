254503886a31f95a26d61ab4044491a8dbabb82c
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-20 12:52:04 -0400

    editing transferlearning_visualsearch_tfdata_tensor.py adding functions

--- Books/Mastering_CV_with_TF2_Packt/chapter6/transferlearning_visualsearch_tfdata_tensor.py
+++ Books/Mastering_CV_with_TF2_Packt/chapter6/transferlearning_visualsearch_tfdata_tensor.py
@@ -39,11 +39,31 @@ for f in train_ds.take(5):
 
 
 ##############################################################################
+def get_label(file_path):
+    # convert the path to a list of path components
+    parts = tf.strings.split(file_path, os.path.sep)
+    # The second to last is the class-directory
+    return parts[-2] == class_list
 
 
+def decode_img(img):
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=3)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize the image to the desired size.
+    return tf.image.resize(img, [img_width, img_height])
 
 
+def process_path(file_path):
+    label = get_label(file_path)
+    # load the raw data from the file as a string
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
 
+
+##############################################################################
 
 
 
