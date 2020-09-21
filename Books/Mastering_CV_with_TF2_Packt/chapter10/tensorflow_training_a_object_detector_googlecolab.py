3da3b459122fd8f0fcbc7c5e0723206c2f2237f5
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-21 10:22:45 -0400

    editing tensorflow_training_a_object_detector_googlecolab.py adding config block

--- Books/Mastering_CV_with_TF2_Packt/chapter10/tensorflow_training_a_object_detector_googlecolab.py
+++ Books/Mastering_CV_with_TF2_Packt/chapter10/tensorflow_training_a_object_detector_googlecolab.py
@@ -2,3 +2,56 @@ import tensorflow as tf
 import os
 
 ##############################################################################
+# Configs and Hyper-parameters
+
+num_steps = 1000  # training steps
+num_eval_steps = 50  # eval steps
+
+MODELS_CONFIG = {
+    'ssd_mobilenet_v2': {
+        'model_name': 'ssd_mobilenet_v2_coco_2018_03_29',
+        'pipeline_file': 'ssd_mobilenet_v2_coco.config',
+        'batch_size': 12
+    },
+    'ssd_inception_v2': {
+        'model_name': 'ssd_inception_v2_coco_2018_01_28',
+        'pipeline_file': 'ssd_inception_v2_coco.config',
+        'batch_size': 12
+    },
+    'faster_rcnn_inception_v2': {
+        'model_name': 'faster_rcnn_inception_v2_coco_2018_01_28',
+        'pipeline_file': 'faster_rcnn_inception_v2_pets.config',
+        'batch_size': 12
+    },
+    'rfcn_resnet101': {
+        'model_name': 'rfcn_resnet101_coco_2018_01_28',
+        'pipeline_file': 'rfcn_resnet101_pets.config',
+        'batch_size': 8
+    }
+}
+
+selected_model = 'rfcn_resnet101'
+
+MODEL = MODELS_CONFIG[selected_model]['model_name']
+pipeline_file = MODELS_CONFIG[selected_model]['pipeline_file']
+batch_size = MODELS_CONFIG[selected_model]['batch_size']
+
+##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
