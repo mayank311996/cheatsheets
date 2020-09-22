e35e4d343a73e476fa7318cf7d2a4f06b1b84ec5
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-22 09:52:36 -0400

    editing main.py adding STEP 2 block

--- customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
+++ customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
@@ -36,3 +36,33 @@ def download_images():
 
 
 IMAGE_PATHS = download_images()
+
+
+##############################################################################
+# STEP 2 Download the model
+def download_model(model_name, model_date):
+    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + model_date + '/' +
+                                               model_file,
+                                        untar=True)
+    return str(model_dir)
+
+
+MODEL_DATE = '20200711'
+MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
+PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
+
+
+
+
+
+
+
+
+
+
+
+
+
