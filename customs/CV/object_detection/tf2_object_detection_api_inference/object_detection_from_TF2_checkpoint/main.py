5a29869972f5fdcd42945d608722ee42f12b8f59
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-22 09:53:57 -0400

    editing main.py adding STEP 3 block

--- customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
+++ customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
@@ -55,6 +55,26 @@ MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
 PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
 
 
+##############################################################################
+# STEP 3 Download the labels
+def download_labels(filename):
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master/' \
+               'research/object_detection/data/'
+    label_dir = tf.keras.utils.get_file(fname=filename,
+                                        origin=base_url + filename,
+                                        untar=False)
+    label_dir = pathlib.Path(label_dir)
+    return str(label_dir)
+
+
+LABEL_FILENAME = 'mscoco_label_map.pbtxt'
+PATH_TO_LABELS = download_labels(LABEL_FILENAME)
+
+
+
+
+
+
 
 
 
