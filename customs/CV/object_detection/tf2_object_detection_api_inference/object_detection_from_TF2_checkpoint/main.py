d3414bdc12725968b49f7b91b031008dfa5f9a82
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-22 09:51:23 -0400

    editing main.py adding import and STEP 1 block

---
+++ customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
@@ -0,0 +1,38 @@
+import os
+import pathlib
+import tensorflow as tf
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+##############################################################################
+# STEP 1 Download the test images
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TensorFlow logging (1)
+tf.get_logger().setLevel('ERROR')  # Suppress TensorFlow logging (2)
+
+# Enable GPU dynamic memory allocation
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def download_images():
+    base_url = 'https://raw.githubusercontent.com/tensorflow/models/master' \
+               '/research/object_detection/test_images/ '
+    filenames = ['image1.jpg', 'image2.jpg']
+    image_paths = []
+    for filename in filenames:
+        image_path = tf.keras.utils.get_file(fname=filename,
+                                             origin=base_url + filename,
+                                             untar=False)
+        image_path = pathlib.Path(image_path)
+        image_paths.append(str(image_path))
+    return image_paths
+
+
+IMAGE_PATHS = download_images()
