dbb57ed8aa00ac8248239e9dd68dfac9ad9154f0
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-22 10:00:13 -0400

    editing main.py adding STEP 4 block

--- customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
+++ customs/CV/object_detection/tf2_object_detection_api_inference/object_detection_from_TF2_checkpoint/main.py
@@ -3,7 +3,9 @@ import pathlib
 import tensorflow as tf
 import time
 from object_detection.utils import label_map_util
+from object_detection.utils import config_util
 from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -70,6 +72,42 @@ def download_labels(filename):
 LABEL_FILENAME = 'mscoco_label_map.pbtxt'
 PATH_TO_LABELS = download_labels(LABEL_FILENAME)
 
+
+##############################################################################
+# STEP 4 Load the model
+PATH_TO_CFG = PATH_TO_MODEL_DIR + "/pipeline.config"
+PATH_TO_CKPT = PATH_TO_MODEL_DIR + "/checkpoint"
+
+print('Loading model... ', end='')
+start_time = time.time()
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(PATH_TO_CFG)
+model_config = configs['model']
+detection_model = model_builder.build(
+    model_config=model_config,
+    is_training=False
+)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(PATH_TO_CKPT, 'ckpt-0')).expect_partial()
+
+
+@tf.function
+def detect_fn(image):
+    """Detect objects in image."""
+
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+
+    return detections
+
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print('Done! Took {} seconds'.format(elapsed_time))
 
 
 
