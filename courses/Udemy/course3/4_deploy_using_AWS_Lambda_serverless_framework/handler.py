2c29f7b0dbbb8fa82b7cf8976da4d471d6000789
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-22 22:16:26 -0400

    editing handler.py

--- courses/Udemy/course3/4_deploy_using_AWS_Lambda_serverless_framework/handler.py
+++ courses/Udemy/course3/4_deploy_using_AWS_Lambda_serverless_framework/handler.py
@@ -1,24 +1,45 @@
 import json
+import pickle
+import numpy as np
 
+model_name = 'flower-v1.pkl'
+model_pk = pickle.load(open(model_name, 'rb'))
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+def predict(event= None, context= None):
+	
+	body = {
+        "message": "OK",
     }
+	
+	if 'queryStringParameters' in event.keys():
+        
+		params = event['queryStringParameters']
+		
+		sepal_length = float(params['sepal_length'])
+		sepal_width = float(params["sepal_width"])
+		petal_length = float(params["petal_length"])
+		petal_width = float(params["petal_width"])
+		
+		data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+		
+		prediction = model_pk.predict(data)
+		
+		body['prediction'] = prediction
+		
+	else:
+	
+		body['message'] = 'queryStringParameters not in event.'
 
-    response = {
+	response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": prediction[0],
+        "headers": {
+            "Content-Type": 'application/json',
+            "Access-Control-Allow-Origin": "*"
+        }
     }
+	
+	return response
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+data = {"queryStringParameters": {"sepal_length": 200000, "sepal_width": 10, "petal_length": 4, "petal_width": 1}}
+print(predict(data))
