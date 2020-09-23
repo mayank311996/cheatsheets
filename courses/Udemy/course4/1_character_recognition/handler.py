3a865d9347cf064745bc47bc871dab42e7262ba3
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-23 16:30:34 -0400

    editing handler.py

--- courses/Udemy/course4/1_character_recognition/handler.py
+++ courses/Udemy/course4/1_character_recognition/handler.py
@@ -1,24 +1,35 @@
 import json
+import boto3
 
+reko = boto3.client('rekognition')
+s3 = boto3.client('s3')
 
 def char_reco(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    print()
+    
+    print(bucket + " :: " + key)
+
+    response=reko.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':key}})
+
+    textDetections=response['TextDetections']
+    print ('Printing Detected text\n----------')
+    for text in textDetections:
+            print ('Detected text:' + text['DetectedText'])
+
+    new_file = key+".txt"        
+    
+    response = s3.put_object(Body=str(textDetections), 
+                      Bucket=bucket, 
+                      Key=new_file)
+
+    print(response)
+
+    
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    
