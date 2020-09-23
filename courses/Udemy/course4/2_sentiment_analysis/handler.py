c6bb80099c72ed3c7cd5e1c6b249284e2e794288
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-23 16:46:30 -0400

    editing handler.py and serverless.yml

--- courses/Udemy/course4/2_sentiment_analysis/handler.py
+++ courses/Udemy/course4/2_sentiment_analysis/handler.py
@@ -1,24 +1,38 @@
 import json
+import boto3
 
+comprehend = boto3.client(service_name='comprehend', region_name='ap-south-1')
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+def detect_sentiment(event, context):
+    
+    print("Dynamo DB Data :: " + str(event))
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    rating = event["Records"][0]['dynamodb']['NewImage']['rating']['S']
+    pid = event["Records"][0]['dynamodb']['NewImage']['pid']['S']
+    text = event["Records"][0]['dynamodb']['NewImage']['text']['S']
+    
+    sentiments = comprehend.detect_sentiment(Text=text, LanguageCode='en')
+
+    print("Sentiments :: " + str(sentiments))
+    
+    print("Sentiment Type : " + str(sentiments["Sentiment"]))
+    print("Score : " + str(sentiments["SentimentScore"]))
+    
+    table = boto3.resource('dynamodb').Table('reviewsp2')
+
+    response = table.update_item(
+        Key={
+            'pid': pid
+        },
+        UpdateExpression='SET score = :val1, sentiment = :val2',
+        ExpressionAttributeValues={
+            ':val1': str(sentiments["SentimentScore"]),
+            ':val2': str(sentiments["Sentiment"])
+        }
+    )
+
+    print("response : " + str(response))
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    
