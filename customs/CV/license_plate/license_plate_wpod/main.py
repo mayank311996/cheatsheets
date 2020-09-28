b7d3342284053dee64e9b223431824ce444d4296
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-28 13:41:16 -0400

    editing main.py adding import block

--- customs/CV/license_plate/license_plate_wpod/main.py
+++ customs/CV/license_plate/license_plate_wpod/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from local_utils import detect_lp
-from os.path import splitext,basename
+from os.path import splitext, basename
 from keras.models import model_from_json
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.mobilenet_v2 import preprocess_input
@@ -13,3 +13,5 @@ import os
 
 # remove warning message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+##############################################################################
