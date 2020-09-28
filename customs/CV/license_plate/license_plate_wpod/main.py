988b99b9a616fbf1103d1bffea87eda4d4d422fe
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-28 14:23:57 -0400

    editing main.py

--- customs/CV/license_plate/license_plate_wpod/main.py
+++ customs/CV/license_plate/license_plate_wpod/main.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import LabelEncoder
 import glob
 import os
 from utils_plate import load_model
+from utils_plate import get_plate
+from utils_plate import emphasize_image
 
 # remove warning message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -19,4 +21,41 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 if __name__ == '__main__':
     wpod_net_path = "input/wpod-net.json"
     wpod_net = load_model(wpod_net_path)
+
+    test_image_path = "Plate_examples/germany_car_plate.jpg"
+    vehicle, LpImg, cor = get_plate(test_image_path)
+
+    binary, thre_mor = emphasize_image(LpImg)
+
+    cont, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL,
+                               cv2.CHAIN_APPROX_SIMPLE)
+
+    # creat a copy version "test_roi" of plat_image to draw bounding box
+    test_roi = plate_image.copy()
+
+    # Initialize a list which will be used to append charater image
+    crop_characters = []
+
+    # define standard width and height of character
+    digit_w, digit_h = 30, 60
+
+    for c in sort_contours(cont):
+        (x, y, w, h) = cv2.boundingRect(c)
+        ratio = h / w
+        if 1 <= ratio <= 3.5:  # Only select contour with defined ratio
+            if h / plate_image.shape[0] >= 0.5:
+                # Select contour which has the height larger
+                # than 50% of the plate
+                # Draw bounding box arroung digit number
+                cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Sperate number and gibe prediction
+                curr_num = thre_mor[y:y + h, x:x + w]
+                curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+                _, curr_num = cv2.threshold(curr_num, 220, 255,
+                                            cv2.THRESH_BINARY +
+                                            cv2.THRESH_OTSU)
+                crop_characters.append(curr_num)
+
+
 
