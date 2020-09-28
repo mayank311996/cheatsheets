5c4d9a0a4b61575f3c9dc0ae9c1e44e5a4a612ec
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-28 13:54:34 -0400

    editing main.py

--- customs/CV/license_plate/license_plate_wpod/main.py
+++ customs/CV/license_plate/license_plate_wpod/main.py
@@ -10,8 +10,13 @@ from keras.applications.mobilenet_v2 import preprocess_input
 from sklearn.preprocessing import LabelEncoder
 import glob
 import os
+from utils_plate import load_model
 
 # remove warning message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 ##############################################################################
+if __name__ == '__main__':
+    wpod_net_path = "input/wpod-net.json"
+    wpod_net = load_model(wpod_net_path)
+
