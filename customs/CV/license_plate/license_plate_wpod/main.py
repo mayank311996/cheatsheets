f73ceff6bc3650df667c019494d893c4f8aed35b
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-28 15:10:33 -0400

    editing main.py fixing bug

--- customs/CV/license_plate/license_plate_wpod/main.py
+++ customs/CV/license_plate/license_plate_wpod/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from local_utils import detect_lp
 from os.path import splitext, basename
-from keras.models import model_from_json
+from tensorflow.keras.models import model_from_json
 from keras.preprocessing.image import load_img, img_to_array
 from keras.applications.mobilenet_v2 import preprocess_input
 from sklearn.preprocessing import LabelEncoder
