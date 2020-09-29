c1bc97e8bf7de6758db1a408b860baaba5490f63
mayank311996 <mayankvadsola1996@gmail.com>
2020-09-29 13:07:36 -0400

    editing app.py adding main block

--- customs/CV/license_plate/license_plate_wpod/streamlit_app/app.py
+++ customs/CV/license_plate/license_plate_wpod/streamlit_app/app.py
@@ -22,3 +22,81 @@ from PIL import Image
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 ##############################################################################
+st.header("Upload here for License Plate Detection")
+
+uploaded_file = st.file_uploader("Choose an image...",
+                                 key="1")  # , type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image for License Plate Detection',
+             width=300)
+    st.write("")
+    st.write("Predicted:")
+
+    # image = load_img(uploaded_file)
+    # image = img_to_array(image)
+
+    wpod_net_path = "input/wpod-net.json"
+    wpod_net = load_model(wpod_net_path)
+
+    # test_image_path = "1.jpg"
+    vehicle, LpImg, cor = get_plate(image, wpod_net)
+
+    if len(LpImg):
+        plate_image, binary, thre_mor = emphasize_image(LpImg)
+        # cv2.imwrite('binary.jpg', binary)
+
+    cont, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL,
+                               cv2.CHAIN_APPROX_SIMPLE)
+    # print(cont)
+
+    # creat a copy version "test_roi" of plat_image to draw bounding box
+    test_roi = plate_image.copy()
+
+    # Initialize a list which will be used to append charater image
+    crop_characters = []
+
+    # define standard width and height of character
+    digit_w, digit_h = 30, 60
+
+    for c in sort_contours(cont):
+        (x, y, w, h) = cv2.boundingRect(c)
+        ratio = h / w
+        if 1 <= ratio <= 3.5:  # Only select contour with defined ratio
+            if h / plate_image.shape[0] >= 0.5:
+                # Select contour which has the height larger
+                # than 50% of the plate
+                # Draw bounding box arroung digit number
+                cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Sperate number and gibe prediction
+                curr_num = thre_mor[y:y + h, x:x + w]
+                curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+                _, curr_num = cv2.threshold(curr_num, 220, 255,
+                                            cv2.THRESH_BINARY +
+                                            cv2.THRESH_OTSU)
+                crop_characters.append(curr_num)
+
+    # for i in range(len(crop_characters)):
+    #     cv2.imwrite('trial' + str(i) + '.jpg', crop_characters[i])
+
+    # Load model architecture, weight and labels
+    json_file = open('input/MobileNets_character_recognition.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    model.load_weights("input/License_character_recognition_weight.h5")
+    print("[INFO] Model loaded successfully...")
+
+    labels = LabelEncoder()
+    labels.classes_ = np.load('input/license_character_classes.npy')
+    print("[INFO] Labels loaded successfully...")
+
+    final_string = ''
+    for i, character in enumerate(crop_characters):
+        title = np.array2string(predict_from_model(character, model, labels))
+        final_string += title.strip("'[]")
+
+    st.write('%s' % final_string)
+
+##############################################################################
