b3c3d95c6980a405fdc117e763941f434bff1c6f
mayank311996 <mayankvadsola1996@gmail.com>
2020-10-05 16:31:13 -0400

    editing app.py fixing bug (need to input only jpg file to model after converting)

--- customs/CV/face_detection_and_recognition/face_detection_ml_mastery/streamlit_app_MTCNN/app.py
+++ customs/CV/face_detection_and_recognition/face_detection_ml_mastery/streamlit_app_MTCNN/app.py
@@ -14,20 +14,21 @@ uploaded_file = st.file_uploader("Choose an image...",
                                  key="1")  # , type="jpg")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    image.save('test.png')
+    image.save('test.jpg')
     st.image(image, caption='Uploaded Image for Face Detection',
              width=300)
     st.write("")
 #    st.write("Predicted:")
 
     # load image from file
-    pixels = pyplot.imread('test.png')
+    pixels = pyplot.imread('test.jpg')
     # create the detector, using default weights
     detector = MTCNN()
     # detect faces in the image
     faces = detector.detect_faces(pixels)
+    print(faces)
     # display faces on the original image
-    draw_image_with_boxes('test.png', faces)
+    draw_image_with_boxes('test.jpg', faces)
     st.write(f"Number of faces: {len(faces)}")
 
 ##############################################################################
