990378c09c0ce9455a95403b303b9183493fa9d3
mayank311996 <mayankvadsola1996@gmail.com>
2020-10-05 11:43:21 -0400

    editing app.py

---
+++ customs/CV/face_detection_and_recognition/face_detection_ml_mastery/streamlit_app_MTCNN/app.py
@@ -0,0 +1,32 @@
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from mtcnn.mtcnn import MTCNN
+import streamlit as st
+from PIL import Image
+from utils_detection import draw_image_with_boxes
+
+##############################################################################
+st.header("Upload here for Face Detection")
+
+uploaded_file = st.file_uploader("Choose an image...",
+                                 key="1")  # , type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    image.save('test.jpg')
+    st.image(image, caption='Uploaded Image for Face Detection',
+             width=300)
+    st.write("")
+#    st.write("Predicted:")
+
+    # load image from file
+    pixels = pyplot.imread('test.png')
+    # create the detector, using default weights
+    detector = MTCNN()
+    # detect faces in the image
+    faces = detector.detect_faces(pixels)
+    # display faces on the original image
+    draw_image_with_boxes('test.png', faces)
+    st.write(f"Number of faces: {len(faces)}")
+
+##############################################################################
