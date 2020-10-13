4896859087ce8aef94bf9dafd35079219786f62b
mayank311996 <mayankvadsola1996@gmail.com>
2020-10-13 11:38:34 -0400

    editing config.py

--- customs/CV/car_make_model/config.py
+++ customs/CV/car_make_model/config.py
@@ -6,15 +6,15 @@ DEBUG = False
 EPOCHS = 10
 MOMENTUM = 0.9
 LEARNING_RATE = 0.01
-BATCH_SIZE = 16
+BATCH_SIZE = 256
 THREADS = 8
 
-USE_CUDA=False
+USE_CUDA = True
 
 # file paths
-IMAGES_PATH = "dataset"
+IMAGES_PATH = "input"
 TRAINING_PATH = "train_file.csv"
 VALIDATION_PATH = "test_file.csv"
 TEST_PATH = "test_file.csv"
 
-RESULTS_PATH="results"
+RESULTS_PATH = "results"
