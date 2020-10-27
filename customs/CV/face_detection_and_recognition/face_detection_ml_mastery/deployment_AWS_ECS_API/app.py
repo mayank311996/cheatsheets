5f11afde1618172a3b5d71d6b894181a93aeaad3
mayank311996 <mayankvadsola1996@gmail.com>
2020-10-27 19:41:08 -0400

    editing app.py

--- customs/CV/face_detection_and_recognition/face_detection_ml_mastery/deployment_AWS_ECS_API/app.py
+++ customs/CV/face_detection_and_recognition/face_detection_ml_mastery/deployment_AWS_ECS_API/app.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot
 from mtcnn.mtcnn import MTCNN
-import streamlit as st
 from PIL import Image
 from utils_detection import draw_image_with_boxes
 from utils_detection import write_to_file
@@ -8,12 +7,14 @@ from utils_detection import allowed_file
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+import io
+import base64
 
 ##############################################################################
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/test'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# UPLOAD_FOLDER = '/test'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 @app.route('/api_predict', methods=['POST', 'GET'])
@@ -21,29 +22,26 @@ def api_predict():
     if request.method == 'GET':
         return "Please Send POST Request"
     elif request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'test.jpg'))
+        im_file = io.BytesIO(request.get_data())
+        img = Image.open(im_file)
+        img.save('test.jpg')
+
+        # file = request.files['image']
+        # file.save('im-received.jpg')
+        ## img = Image.open(file.stream)
+
+        ## print(request.files['file'].read())
+        ## write_to_file('test.jpg', request.files['file'])
 
         # load image from file
-        pixels = pyplot.imread('/test/test.jpg')
+        pixels = pyplot.imread('test.jpg')
         # create the detector, using default weights
         detector = MTCNN()
         # detect faces in the image
         faces = detector.detect_faces(pixels)
         # print(faces)
         # display faces on the original image
-        draw_image_with_boxes('/test/test.jpg', faces)
+        # draw_image_with_boxes('/test/test.jpg', faces)
 
         return str(len(faces))
 
