06646a9c20fe4e24a26e1ca91475e9e70f7a0add
mayank311996 <mayankvadsola1996@gmail.com>
2020-10-30 12:42:23 -0400

    editing utils.py

--- customs/CV/deployment/FastAPI_Object_detection_mlwhiz/utils.py
+++ customs/CV/deployment/FastAPI_Object_detection_mlwhiz/utils.py
@@ -1,6 +1,7 @@
 import base64
 import io
 from PIL import Image
+import requests, json
 
 ##############################################################################
 # To convert image to string
@@ -8,6 +9,7 @@ with open("sample_images/dog_with_ball.jpg", "rb") as image_file:
     base64str = base64.b64encode(image_file.read()).decode("utf-8")
 
 
+##############################################################################
 # To convert string back to image
 def base64str_to_PILImage(base64str):
     base64_img_bytes = base64str.encode('utf-8')
@@ -15,3 +17,18 @@ def base64str_to_PILImage(base64str):
     bytesObj = io.BytesIO(base64bytes)
     img = Image.open(bytesObj)
     return img
+
+
+##############################################################################
+# To send request to deployed API
+payload = json.dumps({
+    "base64str": base64str,
+    "threshold": 0.5
+})
+
+response = requests.put(
+    "[http://127.0.0.1:8000/predict](http://127.0.0.1:8000/predict)",
+    data=payload)
+data_dict = response.json()
+
+##############################################################################
