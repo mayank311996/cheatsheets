739a65f88e285484af99c066cf95bdb97013d6b4
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-02 14:22:35 -0500

    adding more features

--- courses/freecodecamp/JavaScript/main.js
+++ courses/freecodecamp/JavaScript/main.js
@@ -250,8 +250,71 @@ function fun2() {
 fun1();
 fun2();
 
+// ############## Local scope and functions
 
+function myLocalScope() {
+    var myVar = 5;
+    console.log(myVar); // local scope
+}
+myLocalScope();
+console.log(myVar); // Will give an error
 
+// ############## Global vs local scope in functions
+
+// It is possible to have same names for both local and global variables.
+// However, local variable will take over global variable.
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+    var outerWear = "sweater";
+
+    return outerWear;
+}
+
+console.log(myOutfit());
+console.log(outerWear);
+
+// ############## Return a value from a function with return
+
+function minusSeven(num) {
+    return num - 7;
+}
+console.log(minusSeven(10));
+
+function timesFive(num) {
+    return num * 5;
+}
+console.log(timesFive(5));
+
+// ############## Understanding undefined value returned from a function
+
+// Example
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum += 5;
+}
+
+// ############## Assignment with a returned value
+
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+}
+changed = change(10);
+
+var processed = 0;
+function processArg(num) {
+    return (num + 3) / 5;
+}
+processed = processArg(7);
+
+// ############### Stand in line
 
 
 
