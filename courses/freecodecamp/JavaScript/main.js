e6f913ec20b1e4051326458bf711e84f78654d70
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-02 12:46:08 -0500

    adding more features

--- courses/freecodecamp/JavaScript/main.js
+++ courses/freecodecamp/JavaScript/main.js
@@ -92,6 +92,122 @@ console.log(wordBlanks("dog", "big", "ran", "quickly"))
 
 // ################ Store multiple values with arrays
 
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line
+var myArray = ["Quincy", 1];
+
+// ############### Nested arrays
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line
+var myArray = [["Bulls", 23], ["White", 20]];
+
+// ############### Access array data with indexes
+
+// Example
+var ourArray = [50, 60, 70];
+var ourData = ourArray[0];
+
+// Setup
+var myArray = [50, 60, 70];
+
+// Only change code below this line
+var myData = myArray[0];
+console.log(myData)
+
+// ################ Modify array data with indexes
+
+// Example
+var ourArray = [18, 64, 99];
+ourArray[1] = 45;
+
+// Setup
+var myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[2] = 0;
+
+// ############### Access multi-dimensional arrays with indexes
+
+// Setup
+var myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14]];
+
+// Only change code below this line
+var myData = myArray[0][0];
+console.log(myData)
+
+// ############## Manipulate arrays with push
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+
+//Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["kite", 1]);
+
+// ############# Manipulate arrays with pop
+
+// Example
+var ourArray = [1, 2, 3];
+var removedFromOurArray = ourArray.pop();
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+var removedFromMyArray = myArray.pop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
