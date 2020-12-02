8dcd83185cbee930a1fa09aef853638af66627b7
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-02 10:35:31 -0500

    adding javascript tutorial and pycharm files

---
+++ courses/freecodecamp/JavaScript/main.js
@@ -0,0 +1,17 @@
+// ############### Find length of a string
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+
+
