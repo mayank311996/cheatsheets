98466f435854cdf1772ba99d4e1eba6266cf3460
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-03 10:26:35 -0500

    adding more features

--- courses/freecodecamp/JavaScript/main.js
+++ courses/freecodecamp/JavaScript/main.js
@@ -484,8 +484,72 @@ function testElseIf(val) {
 }
 testElseIf(7);
 
+// ############ Chaining if else statements
 
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else {
+        return "Huge";
+    }
+}
+console.log(testSize(7));
 
+// ########### Golf code
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey",
+             "Go Home!"]
+
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes == par - 1) {
+        return names[2];
+    } else if (strokes == par) {
+        return names[3];
+    } else if (strokes == par + 1) {
+        return names[4];
+    } else if (strokes == par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
+    }
+    return "Change Me";
+}
+console.log(golfScore(5, 4));
+
+// ############ Switch statements
+// switch uses strict equality operator inside for comparison
+
+function caseInSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+console.log(caseInSwitch(2));
+
+// ############# Default option in switch statements
 
 
 
