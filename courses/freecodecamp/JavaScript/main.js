9ec9fededc9c9679ba02f0b48a3286d130ccecaf
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-03 21:26:37 -0500

    adding more features

--- courses/freecodecamp/JavaScript/main.js
+++ courses/freecodecamp/JavaScript/main.js
@@ -1173,6 +1173,75 @@ var magic = () => {
 
 const magic = () => new Date(); // Concise anonymous function 2
 
+// #################### Write arrow functions with parameters
+
+var myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+// #################### Write higher order arrow functions
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+const squareList = (arr) => {
+    const squaredIntegers = arr.filter(num => Number.isInteger(num) &&
+    num > 0).map(x => x * x);
+    return squaredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
+
+// ##################### Write higher order arrow functions
+
+const increment = (function() {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})(); // confusing check again
+
+console.log(increment(5, 2));
+console.log(increment(5));
+
+// ##################### Use the rest operator (...) with function parameters
+
+const sum = (function() {
+    return function sum(x, y, z) {
+        const args = [x, y, z];
+        return args.reduce((a, b) => a + b, 0);
+    };
+})(); // check again confusing
+console.log(sum(1, 2, 3));
+
+const sum = (function() {
+    return function sum(...args) {
+        return args.reduce((a, b) => a + b, 0);
+    };
+})(); // Check again confusing
+console.log(sum(1, 2, 3, 4));
+// rest operator allows us to pass any number of arguments
+
+// ####################### Use the spread operator to evaluate arrays in-place
+
+const arr1 = ["JAN", "FEB", "MAR", "APR", "MAY"];
+let arr2;
+(function() {
+    // arr2 = arr1; // this will assign arr1 to arr2 means if you change arr1
+    // it will automatically change arr2
+
+    arr2 = [...arr1]; // this is known as spreading, it individually copies all
+    // elements of arr1 to arr2. So, if we change arr1 then also arr2 will not
+    // change
+    arr1[0] = "potato";
+})();
+console.log(arr2);
+
+// ####################### Use destructing assignment to assign variables from
+// objects
+
+
+
+
 
 
 
