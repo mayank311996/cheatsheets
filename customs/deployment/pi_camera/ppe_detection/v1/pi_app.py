da29ae5f25c2e8345c572b9ad98de26a8af5ec33
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-09 14:11:12 -0500

    editing pi_app.py
    - Adding payload code

--- customs/deployment/pi_camera/ppe_detection/v1/pi_app.py
+++ customs/deployment/pi_camera/ppe_detection/v1/pi_app.py
@@ -12,17 +12,39 @@ from datetime import datetime
 
 ##############################################################################
 
+
+def bytesioObj_to_base64str(bytesObj):
+    return base64.b64encode(bytesObj).decode("utf-8")
+
+
+def base64str_to_Image(base64str):
+    base64_img_bytes = base64str.encode('utf-8')
+    base64bytes = base64.b64decode(base64_img_bytes)
+    with open('trial.jpg', "wb") as f:
+      f.write(base64bytes)
+
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
 binary = open('test.jpg', 'rb').read()
 
-# now = datetime.now()
-# dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+base64str = bytesioObj_to_base64str(binary)
+
+payload = json.dumps({
+    "base64str": base64str,
+    "time": dt_string,
+    "device_id": "fgc_raspberrypi",
+    "company_name": "fgc"
+})
+
 
 # data_dict = {
 #     "image": binary,
 #     "time": dt_string
 # }
 
-response = requests.post("https://i9t9o58074.execute-api.us-east-2.amazonaws.com/dev", data=binary)
+response = requests.post("https://i9t9o58074.execute-api.us-east-2.amazonaws.com/dev", data=payload)
 # print(response)
 response_dict = response.json()
-# print(data_dict)
+print(response_dict)
