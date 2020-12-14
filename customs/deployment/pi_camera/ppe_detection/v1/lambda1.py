531fba1bc2eaf7ad165766e4a52ec3b2bee6e84d
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-14 09:37:05 -0500

    modified lambda1.py
    - taking dates from Lambda1 only. Not RaspberryPi

--- customs/deployment/pi_camera/ppe_detection/v1/lambda1.py
+++ customs/deployment/pi_camera/ppe_detection/v1/lambda1.py
@@ -10,31 +10,39 @@ import base64
 
 import boto3
 
+from datetime import datetime
+
+
 lambda_client = boto3.client('lambda')
+
+now = datetime.now()
+dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
 
 
 def lambda_handler(event, context):
+    
     print("Sending stuff to Lambda_2")
-    event = json.loads(event['body'])
+    event = json.loads(event['body']) 
     inputParams = {
         'device_id': event['device_id'],
-        'company_name': event['company_name'],
-        'time': event['time'],
+        'company_name': event['company_name'], 
+        # 'time': event['time'],
+        'time': dt_string,
         'image': event['base64str']
     }
-
+    
     response = lambda_client.invoke(
-        FunctionName='arn:aws:lambda:us-east-2:882207224999:function:pi_camera_ppe_detection_v1_L_2',
-        InvocationType='RequestResponse',
-        Payload=json.dumps(inputParams)
+        FunctionName = 'arn:aws:lambda:us-east-2:882207224999:function:pi_camera_ppe_detection_v1_L_2',
+        InvocationType = 'RequestResponse',
+        Payload = json.dumps(inputParams)
     )
-
+    
     response = lambda_client.invoke(
-        FunctionName='arn:aws:lambda:us-east-2:882207224999:function:pi_camera_ppe_detection_v1_L_3',
-        InvocationType='RequestResponse',
-        Payload=json.dumps(inputParams)
+        FunctionName = 'arn:aws:lambda:us-east-2:882207224999:function:pi_camera_ppe_detection_v1_L_3',
+        InvocationType = 'RequestResponse',
+        Payload = json.dumps(inputParams)
     )
-
+    
     return {
         "statusCode": 200,
         "body": json.dumps({'Status': "Successful"})
