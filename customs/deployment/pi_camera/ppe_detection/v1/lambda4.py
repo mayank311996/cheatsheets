a10e01870de22964b6bdb8cd285ebc19cec04331
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-14 13:08:03 -0500

    editing lambda4.py
    - adding trial s3 functionality

--- customs/deployment/pi_camera/ppe_detection/v1/lambda4.py
+++ customs/deployment/pi_camera/ppe_detection/v1/lambda4.py
@@ -1,4 +1,7 @@
 import json
+import boto3
+
+s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
@@ -9,6 +12,7 @@ def lambda_handler(event, context):
         for record in event['Records']:
             # 2. Handle event by type
             if record['eventName'] == 'INSERT':
+                print('-----------------entering function 1')
                 handle_insert(record)
         print('------------------------')
         return "Success!"
@@ -17,15 +21,38 @@ def lambda_handler(event, context):
         print('------------------------')
         return "Error"
 
-def handle_insert(record):
 
+def handle_insert(record):
+    print('------------------------entering function 2')
     newImage = record['dynamodb']['NewImage']
     newDeviceId = newImage['device_id']['S']
     newTime = newImage['time']['S']
     newCompanyName = newImage['company']['S']
     newPredictions = newImage['predictions']['S']
 
+    print('------------------------printing')
     print(newDeviceId)
     print(newTime)
     print(newCompanyName)
     print(newPredictions)
+    print('------------------------end printing')
+
+    event = json.loads(newPredictions)
+
+    print("File loading from s3")
+    input_file_path = newCompanyName + "/" + newDeviceId + "/" \
+                      + str(newTime.split()[0]) + "/" + str(
+        '_'.join(newTime.split()[1].split(':'))) + ".jpg"
+
+    with open("/tmp/photo.jpg", 'wb') as f:
+        s3.download_fileobj("pi-camera-ppe-detection-s3", input_file_path, f)
+    print("File loaded from s3")
+
+    print("Writing file to s3")
+    output_file_path = newCompanyName + "/" + newDeviceId + "/" \
+                      + str(newTime.split()[0]) + "/" + str(
+        '_'.join(newTime.split()[1].split(':'))) + ".jpg"
+
+    with open("/tmp/photo.jpg", "rb") as f:
+        s3.upload_fileobj(f, "pi-camera-ppe-detection-prediction-s3", output_file_path)
+    print("file written to s3")
