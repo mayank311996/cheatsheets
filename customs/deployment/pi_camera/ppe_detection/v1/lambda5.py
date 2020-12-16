f843c6b97fcdcfcc0c36ba14a85134a5e9259bb4
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-16 09:46:04 -0500

    editing lambda5.py
    - some errors trial

--- customs/deployment/pi_camera/ppe_detection/v1/lambda5.py
+++ customs/deployment/pi_camera/ppe_detection/v1/lambda5.py
@@ -2,13 +2,13 @@ import json
 import boto3
 import base64
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 
 def lambda_handler(event, context):
 
     print("Sending stuff to Lambda_2")
-    event = json.loads(event['body'])
+    # event = json.loads(event['body'])
 
     device_id = event['device_id']
     company_name = event['company_name']
@@ -25,14 +25,15 @@ def lambda_handler(event, context):
     prediction_list = []
 
     for obj in bucket_raw.objects.filter(Prefix=find_prefix):
+        print(obj)
         if obj.size:
-            binary = open(obj.key, 'rb').read()
+            binary = obj.get()['Body'].read()
             base64str = bytesioObj_to_base64str(binary)
             raw_list.append(base64str)
 
     for obj in bucket_predictions.objects.filter(Prefix=find_prefix):
         if obj.size:
-            binary = open(obj.key, 'rb').read()
+            binary = open("s3://pi-camera-ppe-detection-prediction-s3/" + obj.key, 'rb').read()
             base64str = bytesioObj_to_base64str(binary)
             prediction_list.append(base64str)
 
