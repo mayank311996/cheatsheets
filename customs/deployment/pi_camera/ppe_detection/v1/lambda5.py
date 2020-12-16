8e6f6fe2a7d405057f927bded4d11d5dc649f001
mayank311996 <mayankvadsola1996@gmail.com>
2020-12-16 09:17:20 -0500

    editing lambda5.py
    - trial lambda function

---
+++ customs/deployment/pi_camera/ppe_detection/v1/lambda5.py
@@ -0,0 +1,47 @@
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+    print("Sending stuff to Lambda_2")
+    event = json.loads(event['body'])
+
+    device_id = event['device_id']
+    company_name = event['company_name']
+    date = event['date']
+    hour = event['hour']
+
+    find_prefix = company_name + "/" + device_id + "/" \
+                      + date + "/" + hour + "_"
+
+    bucket_raw = s3.Bucket("pi-camera-ppe-detection-s3")
+    bucket_predictions = s3.Bucket("pi-camera-ppe-detection-prediction-s3")
+
+    raw_list = []
+    prediction_list = []
+
+    for obj in bucket_raw.objects.filter(Prefix=find_prefix):
+        if obj.size:
+            binary = open(obj.key, 'rb').read()
+            base64str = bytesioObj_to_base64str(binary)
+            raw_list.append(base64str)
+
+    for obj in bucket_predictions.objects.filter(Prefix=find_prefix):
+        if obj.size:
+            binary = open(obj.key, 'rb').read()
+            base64str = bytesioObj_to_base64str(binary)
+            prediction_list.append(base64str)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({'raw_list': raw_list,
+                            'prediction_list': prediction_list})
+    }
+
+
+def bytesioObj_to_base64str(bytesObj):
+    return base64.b64encode(bytesObj).decode("utf-8")
