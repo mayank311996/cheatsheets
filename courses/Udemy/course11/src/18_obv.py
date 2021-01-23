d2a3c82715155cac970f45f06bf336ec1f0abf5e
mayank311996 <mayankvadsola1996@gmail.com>
2021-01-23 17:55:58 -0500

    adding script 18

---
+++ courses/Udemy/course11/src/18_obv.py
@@ -0,0 +1,18 @@
+# Import necesary libraries
+import yfinance as yf
+import numpy as np
+import datetime as dt
+
+# Download historical data for required stocks
+ticker = "AAPL"
+ohlcv = yf.download(ticker,dt.date.today()-dt.timedelta(1825),dt.datetime.today())
+
+def OBV(DF):
+    """function to calculate On Balance Volume"""
+    df = DF.copy()
+    df['daily_ret'] = df['Adj Close'].pct_change()
+    df['direction'] = np.where(df['daily_ret']>=0,1,-1)
+    df['direction'][0] = 0
+    df['vol_adj'] = df['Volume'] * df['direction']
+    df['obv'] = df['vol_adj'].cumsum()
+    return df['obv']
